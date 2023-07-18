e0f10e3b4c4ef72b036c9694b95a7cfa0e293fb3
Hsiao-yuan <littlecircle7171@gmail.com>
2023-07-19 01:26:46 +0800

    add location

--- getDeviceInfo.sh
+++ getDeviceInfo.sh
@@ -1,3 +1,57 @@
+#!/bin/bash
+
+csv_file="location.csv"
+deg2rad() {
+    local degrees="$1"
+    local pi=$(echo "scale=10; 4*a(1)" | bc -l)
+    local radians=$(echo "scale=10; $degrees * $pi / 180" | bc -l)
+    echo "$radians"
+}
+get_distance() {
+    local myIp="$1"
+    local otherIp="$2"
+
+    # Extract the second and third columns of the matched row
+    my_location=$(awk -F',' -v value="$myIp" '$1 == value {print $2, $3}' "$csv_file")
+    other_location=$(awk -F',' -v value="$otherIp" '$1 == value {print $2, $3}' "$csv_file")
+
+    # # Print variable declarations with values
+    # declare -p my_location
+    # declare -p other_location
+    
+    distance=""
+    if [ -n "$my_location" ] && [ -n "$other_location" ]; then
+        local lat1=$(echo "$my_location" | cut -d ' ' -f 1)
+        local lon1=$(echo "$my_location" | cut -d ' ' -f 2)
+        local lat2=$(echo "$other_location" | cut -d ' ' -f 1)
+        local lon2=$(echo "$other_location" | cut -d ' ' -f 2)
+
+        local R=6371  # Radius of the Earth in kilometers
+
+        # Convert latitude and longitude from degrees to radians
+        local lat1_rad=$(deg2rad "$lat1")
+        local lon1_rad=$(deg2rad "$lon1")
+        local lat2_rad=$(deg2rad "$lat2")
+        local lon2_rad=$(deg2rad "$lon2")
+
+        # Calculate the differences in latitude and longitude
+        local dlat=$(echo "$lat2_rad - $lat1_rad" | bc -l)
+        local dlon=$(echo "$lon2_rad - $lon1_rad" | bc -l)
+
+        # Calculate the Haversine formula components
+        local a=$(echo "s(0.5 * $dlat)^2 + c($lat1_rad) * c($lat2_rad) * s(0.5 * $dlon)^2" | bc -l)
+        local c=$(echo "2 * a( sqrt($a) )" | bc -l)
+
+        # Calculate the distance
+        local distance=$(echo "$R * $c" | bc -l)
+
+        # Round the distance to the 4th decimal place
+        distance=$(printf "%.4f" "$distance")
+    fi
+    echo "$distance"
+}
+
+
 # name, cpu, memory, 
 node_info=$(echo "$(kubectl describe nodes)" | grep -E -A 8 '^Name:|^Allocatable:' | grep -E '^Name:|^  cpu:|^  memory:|^  ephemeral-storage:')
 
@@ -10,21 +64,28 @@ while IFS= read -r line; do
         
         # latency
         podsOfCurNode=$( kubectl get pods -l app=m-device-info --field-selector=spec.nodeName==$name --template='{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+        ipOfCurNode=$( kubectl get pods -l app=m-device-info --field-selector=spec.nodeName==$name --template='{{range .items}}{{.status.podIP}}{{"\n"}}{{end}}')
         podsOfOtherNode=$( kubectl get pods -l app=m-device-info --field-selector=spec.nodeName!=$name -o=jsonpath='{range .items[*]}{.status.podIP} {.metadata.name}{"\n"}{end}')
-        # podsIPOfOtherNode=$( kubectl get pods -l app=m-device-info --field-selector=spec.nodeName!=$name -o jsonpath='{range .items[*]}{.status.podIP}{"\n"}{end}')
-        latency="{"
+        latency=""
+        location=""
         if [ -n "$podsOfCurNode" ]; then
+            latency+="{"
+            location+="{"
             while IFS= read -r otherPods; do
                 read -r otherPodsIP otherPodsName <<< "$otherPods"
                 otherNodeName=$(kubectl get pod "$otherPodsName" -o=jsonpath='{.spec.nodeName}')
                 latencyRaw=$(kubectl exec $podsOfCurNode -- sh -c "ping -c 5 $otherPodsIP")
                 latencyTemp=$(echo "$latencyRaw" | grep -E '^round-trip' | awk -F'/' '{print $(NF-1)}')
                 latency+="\"$otherNodeName\":\"$latencyTemp\","
+                #location
+                distance=$(get_distance "$ipOfCurNode" "$otherPodsIP")
+                location+="\"$otherNodeName\":\"$distance\","
             done <<< "$podsOfOtherNode"
             latency=${latency%,}
+            location=${location%,}
             latency+="}"
+            location+="}"
         fi
-
     elif (( count % 4 == 1 )); then
         # cpu
         cpu=$(echo "$line" | sed 's/cpu://' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//') # remove leading and trailing whitespace
@@ -36,11 +97,12 @@ while IFS= read -r line; do
         # ram
         ram=$(echo "$line" | sed 's/memory://' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'  | sed -e 's/..$//') # remove last two character "Ki"
         ram=$(echo "scale=2; "$ram" / 1024" | bc) #convert Ki to MB
-        output+="\n\"$name\":{\n\"cpu\":\"$cpu\", \"ram\":\"$ram\", \"storage\":\"$storage\", \"latency\":\"$latency\"\n},"
+        # output
+        output+="\n\"$name\":{\n\"cpu\":\"$cpu\", \n\"ram\":\"$ram\", \n\"storage\":\"$storage\", \n\"latency\":\"$latency\",\n\"location\":\"$location\"\n},"
     fi
     (( count++ ))
 done <<< "$node_info"
 
 output=${output%,}  # Remove the trailing comma
 output+="\n}"
-echo "$output" > output_device_info.json
+echo -e "$output" > output_device_info.json
